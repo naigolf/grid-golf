77c75081ef44264b3f0e1bcbeff90cdd899f142d
nai golf <niy_golf@hotmail.com>
2026-01-13 14:20:25 +0700

    Update telegram.js

--- telegram.js
+++ telegram.js
@@ -1,12 +1,10 @@
 import axios from "axios";
 import config from "./config.js";
 
-export async function notify(message) {
+export async function notify(text) {
+  if (!config.TELEGRAM_TOKEN || !config.TELEGRAM_CHAT_ID) return;
   await axios.post(
     `https://api.telegram.org/bot${config.TELEGRAM_TOKEN}/sendMessage`,
-    {
-      chat_id: config.TELEGRAM_CHAT_ID,
-      text: message
-    }
+    { chat_id: config.TELEGRAM_CHAT_ID, text }
   );
 }
